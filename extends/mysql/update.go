a052e0aa514c7b2d84e54d137cde508cfb7150f2
yehaoyang <dsreshiram@gmail.com>
2021-09-08 17:48:14 +0800

    update

---
+++ extends/mysql/update.go
@@ -0,0 +1,18 @@
+package mysql
+
+type update struct {
+	table interface{}
+}
+
+func DBUpdate(table interface{}) *update {
+	return &update{table}
+}
+
+// Save 更新数据库
+func (u *update) Save() error {
+	return DB.Self.Save(u.table).Error
+}
+
+func (u *update) Updates(where interface{}, data interface{}) error {
+	return DB.Self.Model(u.table).Where(where).Updates(data).Error
+}
